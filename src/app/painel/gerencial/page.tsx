1722e2279717301614082a412d579b1a42f4cec0
Eryk-dev <eryk@netair.com.br>
2026-03-17 23:47:07 -0300

    Separa painel operacional e gerencial

---
+++ src/app/painel/gerencial/page.tsx
@@ -0,0 +1,5 @@
+import { PainelScreen } from "../_dashboard";
+
+export default function PainelGerencialPage() {
+  return <PainelScreen mode="gerencial" />;
+}
---
+++ src/app/painel/operacao/page.tsx
@@ -0,0 +1,5 @@
+import { PainelScreen } from "../_dashboard";
+
+export default function PainelOperacaoPage() {
+  return <PainelScreen mode="operacao" />;
+}
